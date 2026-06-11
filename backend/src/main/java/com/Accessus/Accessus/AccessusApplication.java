8a58e6d1217adbad372db9808614588b05348d29
Gabriel Oliveira <gabriel.oliveira@solucoesabc.com.br>
2026-06-11 12:43:59 -0300

    Fix: exclude UserDetailsServiceAutoConfiguration via properties, disable httpBasic

--- backend/src/main/java/com/Accessus/Accessus/AccessusApplication.java
+++ backend/src/main/java/com/Accessus/Accessus/AccessusApplication.java
@@ -2,9 +2,8 @@ package com.Accessus.Accessus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication
 public class AccessusApplication {
 
 	public static void main(String[] args) {
